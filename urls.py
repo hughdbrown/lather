e7eeb7fb63e0b48eddbb8c5a96d7d04b6dd025d8
Hugh Brown <hughdbrown@yahoo.com>
2009-07-09 14:46:22 -0400

    Support for serving static content

--- urls.py
+++ urls.py
@@ -6,8 +6,8 @@ from django.conf import settings
 feeds = {
     'posts': LatestArtclesFeed,
     'tags': TaggedArticlesFeed,
-	  'sections': SectionFeed,
-	  'comments': CommentsFeed
+    'sections': SectionFeed,
+    'comments': CommentsFeed
 }
 
 admin.autodiscover()
@@ -16,36 +16,22 @@ import mobileadmin
 mobileadmin.autoregister()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^lather/', include('lather.foo.urls')),
-
-	 url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-	 url(r'^admin/meowr/article/(?P<object_id>[0-9]+)/preview/$', 'meowr.views.preview'),
-	
-	 url(r'^admin/(.*)', admin.site.root),
-	 
-	 url(r'^ma/(.*)', mobileadmin.sites.site.root),
-	
-     #url(r'^admin/', include('django.contrib.admin.urls')),
-
-	 url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', { 'feed_dict': feeds }, name='feeds'),
-
-	 url(r'^(?P<url>(girl|site)/)$', 'django.contrib.flatpages.views.flatpage'), 
-	
-	 url(r'^threadedcomments/', include('threadedcomments.urls')),
-	
-	 url (r'^search/$', 'search.views.search'),
-
-	 url(r'^', include('meowr.urls')),
-	 
+	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	url(r'^admin/meowr/article/(?P<object_id>[0-9]+)/preview/$', 'meowr.views.preview'),
+	url(r'^admin/',  include(admin.site.urls)),
+	url(r'^ma/(.*)', mobileadmin.sites.site.root),
+	url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', { 'feed_dict': feeds }, name='feeds'),
+	url(r'^(?P<url>(girl|site)/)$', 'django.contrib.flatpages.views.flatpage'), 
+	url(r'^threadedcomments/', include('threadedcomments.urls')),
+	url (r'^search/$', 'search.views.search'),
+	url(r'^', include('meowr.urls')),
 )
-
 
 handler404 = 'mobileadmin.views.page_not_found'
 handler500 = 'mobileadmin.views.server_error'
 
-if settings.DEBUG:
-	 urlpatterns += patterns('',
-	 url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/Library/Python/2.5/site-packages/spectrum/static'}),
-)
+if settings.SERVE_MEDIA:
+    urlpatterns += patterns('',
+        url(regex=r'^static/(?P<path>.*)$', view='django.views.static.serve', kwargs={'document_root': settings.MEDIA_ROOT}),
+        url(regex='^media/(?P<path>.*)$',   view='django.views.static.serve', kwargs={'document_root': settings.ADMIN_MEDIA_ROOT}),
+    )
